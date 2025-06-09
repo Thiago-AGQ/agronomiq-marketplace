07b5e1dc35e6af566d06cf2b08f033a91940d397
Thiago-AGQ <thiago.pereira@agronomiq.com.br>
2025-06-09 11:45:36 -0300

    first commit

---
+++ AgronomiQ-Marketplace/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Marketplace from './components/Marketplace.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <Marketplace />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
